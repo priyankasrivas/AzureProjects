1d7bc44dfa8f7ecda66d6e3cde54ebec07bd5f2a
priyankasrivas <sbesbe@1>
2015-03-27 15:55:14 +0530

    New Code

---
+++ GitHub1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHub1.Startup))]
+namespace GitHub1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
